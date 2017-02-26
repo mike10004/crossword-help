3028dc09339ace09ff200a70b7b7d4cee79870e3
mike <mchaberski@gmail.com>
2017-02-26 15:27:13 -0500

    deploy to gh-pages dir

--- dist/app.js
+++ dist/app.js
@@ -3,10 +3,10 @@
 // Declare app level module which depends on views, and components
 angular.module('crosswordHelpApp', [
   'ngRoute',
-  'crosswordHelpApp.view1'
+  'crosswordHelpApp.home'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/home'});
 }]);
---
+++ dist/components/warehouse.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('crosswordHelpApp').factory('Warehouse', ['$http', function($http) {
+    class Warehouse {
+        constructor() {
+            this.fake = false;
+        }
+
+        fetch() {
+            return new Promise(function(resolve, reject){
+                $http.get('data/words.json')
+                  .then(response => resolve(response.data))
+                  .catch(response => reject(response));
+            }); 
+        }
+    }
+    return new Warehouse();
+}]);
