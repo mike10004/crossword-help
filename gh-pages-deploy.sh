1e07f33ebdc95293df6131c2e690eb34e6801773
mike <mchaberski@gmail.com>
2017-02-26 15:33:48 -0500

    add subtree push to deploy script

--- gh-pages-deploy.sh
+++ gh-pages-deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-set -e`
+set -e
 
 rm -rf dist
 cp -r app dist
 find dist -type f -name "*_test.js" -delete
-`
+git add dist && git commit dist -m 'deploy to gh-pages dir'
+# https://gist.github.com/cobyism/4730490
+git subtree push --prefix dist origin gh-pages
