414095134c5a427e8a6b5b418e2d828516dda8b8
mike <mchaberski@gmail.com>
2017-03-01 23:22:40 -0500

    deploy to gh-pages dir

--- components/cell.js
+++ components/cell.js
@@ -4,7 +4,7 @@ angular.module('crosswordHelpApp')
         model: '<'
     },
     templateUrl: 'components/cell.html',
-    controller: ['Log', 'KeyEvents', function CellController(Log, KeyEvents) {
+    controller: ['Log', 'KeyEvents', '$document', function CellController(Log, KeyEvents, $document) {
         const self = this;
 
         self.removeClicked = function() {
@@ -30,14 +30,17 @@ angular.module('crosswordHelpApp')
 
         self.keyDown = function($event) {
             Log.debug("CellController.keyDown", $event.key);
-            $event.preventDefault();
-            if (KeyEvents.isBlankish($event)) {
-                self.model.value = '';
-            } else if (KeyEvents.isLetter($event)) {
-                self.model.value = $event.key;
+            // TODO: only preventDefault if target has focus and no modifier key is down
+            if ($document[0].activeElement === $event.target && !KeyEvents.hasCtrlOrAlt($event)) {            
+                $event.preventDefault();
+                if (KeyEvents.isBlankish($event)) {
+                    self.model.value = '';
+                } else if (KeyEvents.isLetter($event)) {
+                    self.model.value = $event.key;
+                }
+                const movement = toMovement($event);
+                self.playgroundCtrl.keyed($event, self.model, movement);
             }
-            const movement = toMovement($event);
-            self.playgroundCtrl.keyed($event, self.model, movement);
         };
 
         self.focused = function($event) {
